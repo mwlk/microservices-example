f5f888ecf769aba39c4acaacc99ef177792b0812
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-05-26 22:23:29 -0300

    add books context

---
+++ TiendaServicios.api.book/Models/Library.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TiendaServicios.api.book.Models
+{
+    public class Library
+    {
+        public Guid? LibraryGuidId { get; set; }
+        public string Title { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public Guid? AuthorBook { get; set; }
+
+    }
+}
---
+++ TiendaServicios.api.book/Persistence/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaServicios.api.book.Models;
+
+namespace TiendaServicios.api.book.Persistence
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Library> Libreria { get; set; }
+    }
+}
